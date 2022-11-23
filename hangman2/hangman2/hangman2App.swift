3be99f92b63049215a0f2dfec80b32e16e4bcb5f
kenni kollemorten <96551540+kenn7575@users.noreply.github.com>
2022-11-23 10:50:45 +0100

    Add files via upload

---
+++ hangman2/hangman2/hangman2App.swift
@@ -0,0 +1,17 @@
+//
+//  hangman2App.swift
+//  hangman2
+//
+//  Created by Kenni Kollemorten on 17/08/2021.
+//
+
+import SwiftUI
+
+@main
+struct hangman2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
